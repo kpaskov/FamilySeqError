4826fc435110503aa7a3501b2b63f1a076b58114
Kelley <kpaskov@stanford.edu>
2020-06-18 12:40:15 -0700

    speeding up pull

--- preprocessing/pull_gen_data.py
+++ preprocessing/pull_gen_data.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.sparse import csc_matrix, save_npz
 import time
 import gzip
-from itertools import product
+from itertools import product, islice
 import sys
 import argparse
 
@@ -20,8 +20,12 @@ t0 = time.time()
 
 chrom_int = 23 if args.chrom == 'X' else 24 if args.chrom == 'Y' else 25 if args.chrom == 'MT' else int(args.chrom)
 
+gen_mapping = {'./.': -1, '0/0': 0, '0|0': 0, '0/1': 1, '0|1': 1, '1/0': 1, '1|0': 1, '1/1': 2, '1|1': 2}
+
 # Pull data from vcf
 def process_vcf(f):
+    data, indices, indptr, index = np.zeros((args.maxsize,), dtype=np.int8), np.zeros((args.maxsize,), dtype=int), [0], 0
+
     with gzip.open('%s/chr.%s.%d.gen.variants.txt.gz' % (args.out_directory, args.chrom, args.batch_num), 'wt') as variant_f:
         # Skip header
         line = next(f)
@@ -36,9 +40,7 @@ def process_vcf(f):
 
         # Pull genotypes from vcf
         m = len(sample_ids)
-        data, indices, indptr, index = np.zeros((args.maxsize,), dtype=np.int8), np.zeros((args.maxsize,), dtype=int), [0], 0
-        gen_mapping = {'./.': -1, '0/0': 0, '0|0': 0, '0/1': 1, '0|1': 1, '1/0': 1, '1|0': 1, '1/1': 2, '1|1': 2}
-
+        
         # enumerate all chrom options
         chrom_options = [args.chrom, 'chr'+args.chrom]
         if args.chrom == 'X':
@@ -50,44 +52,39 @@ def process_vcf(f):
         
         line = next(f)
         num_lines_in_file = 0
-        current_line_index = 0
         chrom_coord = []
-        for line in f:
+        for line in islice(f, args.batch_num*args.batch_size, (args.batch_num+1)*args.batch_size):
             pieces = line.split('\t', maxsplit=1)
 
             if pieces[0] in chrom_options:
-                if current_line_index >= (args.batch_num+1)*args.batch_size:
-                    break
-                elif current_line_index >= args.batch_num*args.batch_size:
-                    pieces = line.strip().split('\t')
-                    format = pieces[8].strip().split(':')
+                pieces = line.strip().split('\t')
+                format = pieces[8].strip().split(':')
 
-                    # Write variant to file
-                    variant_f.write('\t'.join(pieces[:9]) + '\n')
+                # Write variant to file
+                variant_f.write('\t'.join(pieces[:9]) + '\n')
 
-                    pos, _, ref, alt = pieces[1:5]
-                    is_biallelic_snp = 1 if len(ref) == 1 and len(alt) == 1 and ref != '.' and alt != '.' else 0
-                    is_pass = pieces[6] == 'PASS'
-                    chrom_coord.append((chrom_int, int(pos), is_biallelic_snp, is_pass))
+                pos, _, ref, alt = pieces[1:5]
+                is_biallelic_snp = 1 if len(ref) == 1 and len(alt) == 1 and ref != '.' and alt != '.' else 0
+                is_pass = pieces[6] == 'PASS'
+                chrom_coord.append((chrom_int, int(pos), is_biallelic_snp, is_pass))
 
-                    # Pull out genotypes
-                    gen_index = format.index('GT')
-                    for i, piece in enumerate(pieces[9:]):
-                        segment = piece.split(':', maxsplit=gen_index+1)
-                        gt = gen_mapping.get(segment[gen_index], -1) # For now we mark multi-base loci as unknown
+                # Pull out genotypes
+                gen_index = format.index('GT')
+                for i, piece in enumerate(pieces[9:]):
+                    segment = piece.split(':', maxsplit=gen_index+1)
+                    gt = gen_mapping.get(segment[gen_index], -1) # For now we mark multi-base loci as unknown
 
-                        if gt != 0:
-                            indices[index] = i
-                            data[index] = gt
-                            index += 1
-                    indptr.append(index)
-                    num_lines_in_file += 1
+                    if gt != 0:
+                        indices[index] = i
+                        data[index] = gt
+                        index += 1
+                indptr.append(index)
+                num_lines_in_file += 1
 
-                    # If file has gotten really big, raise error.
-                    if index+m >= args.maxsize:
-                        raise Exception('Maxsize too low. Increase maxsize and rerun.')
+                # If file has gotten really big, raise error.
+                if index+m >= args.maxsize:
+                    raise Exception('Maxsize too low. Increase maxsize and rerun.')
 
-                current_line_index += 1
 
     if index>0:
         gen = csc_matrix((data[:index], indices[:index], indptr), shape=(m, num_lines_in_file), dtype=np.int8)
