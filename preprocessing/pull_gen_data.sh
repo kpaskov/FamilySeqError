4e17e6477bd3a93df4386721d68d0c18a9be059e
Kelley Paskov <kpaskov@stanford.edu>
2020-06-18 16:58:33 -0700

    fixing up scripts

---
+++ preprocessing/pull_gen_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+#
+#SBATCH --job-name=pull
+#SBATCH --output=logs/pull_%a.out
+#SBATCH --error=logs/pull_%a.err
+#SBATCH --array=1-22%5
+#SBATCH -p dpwall
+#SBATCH -D /oak/stanford/groups/dpwall/users/kpaskov/FamilySeqError
+#SBATCH -t 10:00:00
+#SBATCH --mem=8G
+
+module load py-numpy/1.14.3_py36
+module load py-scipy/1.1.0_py36
+
+# Print this sub-job's task ID
+echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
+
+#srun python3 preprocessing/pull_gen_data.py /scratch/PI/dpwall/DATA/iHART/vcf/mssng/mssng_db6.chr$SLURM_ARRAY_TASK_ID.recal.vcf.gz ../DATA/mssng/genotypes $SLURM_ARRAY_TASK_ID --batch_size 1000000 --batch_num $1
+
+srun python3 preprocessing/pull_gen_data.py /oak/stanford/groups/dpwall/simons_spark/snp/SPARK.30K.array_genotype.20190818.phaseable.passing.vcf ../DATA/spark/genotypes $SLURM_ARRAY_TASK_ID
