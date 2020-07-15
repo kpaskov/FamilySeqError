82ebe1db4e5be8ae131e78c5a3ef28d7daa85001
Kelley <kpaskov@stanford.edu>
2020-07-14 17:08:49 -0700

    fixing up the check

--- preprocessing/check_pull_complete.py
+++ preprocessing/check_pull_complete.py
@@ -1,40 +1,32 @@
 import argparse
-from pysam import VariantFile
 import os.path
 import math
+import json
 
 parser = argparse.ArgumentParser(description='Pull genotypes.')
-parser.add_argument('vcf_file', type=str, help='VCF file to pull from.')
 parser.add_argument('data_dir', type=str, help='Directory containing genotype data.')
 parser.add_argument('file_type', type=str, help='Type of file to check.')
-parser.add_argument('--batch_size', type=int, default=None, help='Restrict number of positions per file to batch_size.')
 args = parser.parse_args()
 
 chroms = [str(x) for x in range(1, 23)]
 
-def process_header(vcf):
-    return sample_ids, vcf.header.contigs
+# grab assembly
+with open('%s/info.json' % args.data_dir, 'r') as f:
+	info = json.load(f)
+	assembly = info['assembly']
+	batch_size = info['batch_size']
 
-vcf = VariantFile(args.vcf_file)
-contigs = vcf.header.contigs
+with open('data/chrom_lengths%s.json' % assembly, 'r') as f:
+	chrom_lengths = json.load(f)
 
 missing_files = []
 for chrom in chroms:
-	# pull contig length
-	contig = None
-	if chrom in contigs:
-	    contig = contigs[chrom]
-	elif 'chr%s' % chrom in contigs:
-	    contig = contigs['chr%s' % chrom]
-	else:
-	    raise Exception('Trouble finding contig', chrom, 'in', contig_names)
-	print('Chrom %s length %d' % (chrom, contig.length))
+	chrom_length = chrom_lengths[chrom]
 
-
-	if args.batch_size is None:
+	if batch_size is None:
 		num_batches = 1
 	else:
-		num_batches = math.ceil(contig.length/args.batch_size)
+		num_batches = math.ceil(chrom_length/batch_size)
 		
 
 	for batch_num in range(num_batches):
