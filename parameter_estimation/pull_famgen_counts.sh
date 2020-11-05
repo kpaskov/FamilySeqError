a3751275088248aa7152fdde82a3820016de0aac
Kelley <kpaskov@stanford.edu>
2020-11-05 10:44:07 -0700

    updating .sh scripts

--- parameter_estimation/pull_famgen_counts.sh
+++ parameter_estimation/pull_famgen_counts.sh
@@ -18,43 +18,44 @@ echo "My SLURM_ARRAY_TASK_ID is " $SLURM_ARRAY_TASK_ID
 
 # ------------------------ For parameter estimation paper -----------------------
 
-# ihart
-#srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.ms2/genotypes ../DATA/ihart.ms2/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.ms2/family_genotype_counts
+# identical twins
+# srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/ihart_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_identicals
+# srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_spark_identicals
+# srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark_exome_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX_identicals --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed
 
-#srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.ms2/genotypes ../DATA/ihart.ms2/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.ms2/family_genotype_counts/HCR --exclude data/btu356-suppl_data/btu356_LCR-hs38.bed/btu356_LCR-hs38.bed
+# sequencing platform
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.ms2/genotypes ../DATA/ihart.ms2/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.ms2/family_genotype_counts
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase1-1 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase1-1
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase1-2 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase1-2              
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase1-3 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase1-3
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase1-4 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase1-4
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase1-5 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase1-5
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase1-7 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase1-7
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase2 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase2
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase2_B01 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase2_B01
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase2_Replacements ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase2_Replacements
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase3_1 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase3_1
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase3_1_B02 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase3_1_B02
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase3_2 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase3_2
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/phase4 ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/phase4
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ssc.hg38/genotypes/pilot ../DATA/ssc.hg38/ssc.ped $SLURM_ARRAY_TASK_ID ../DATA/ssc.hg38/family_genotype_counts/pilot
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/spark.exome/genotypes ../DATA/spark.exome/spark.ped $SLURM_ARRAY_TASK_ID ../DATA/spark.exome/family_genotype_counts/EX --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed 
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.chip/genotypes ../DATA/ihart.chip/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.chip/family_genotype_counts
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/spark/genotypes ../DATA/spark/spark.ped $SLURM_ARRAY_TASK_ID ../DATA/spark/family_genotype_counts
 
-#srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.ms2/genotypes ../DATA/ihart.ms2/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.ms2/family_genotype_counts/LCR --include data/btu356-suppl_data/btu356_LCR-hs38.bed/btu356_LCR-hs38.bed
+# LCR vs HCR
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.ms2/genotypes ../DATA/ihart.ms2/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.ms2/family_genotype_counts/HCR --exclude data/btu356-suppl_data/btu356_LCR-hs38.bed/btu356_LCR-hs38.bed
+# srun python3 parameter_estimation/pull_famgen_counts.py ../DATA/ihart.ms2/genotypes ../DATA/ihart.ms2/ihart.ped $SLURM_ARRAY_TASK_ID ../DATA/ihart.ms2/family_genotype_counts/LCR --include data/btu356-suppl_data/btu356_LCR-hs38.bed/btu356_LCR-hs38.bed
 
-# ihart (GATK 3.2)
+# GATK 3.2 vs GARK 3.4
 # srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart3.2 data/v34.vcf.ped $SLURM_ARRAY_TASK_ID split_gen_ihart3.2
 
-# ihart identicals
-# srun python3 parameter_estimation/pull_famgen_counts.py split_gen_ihart data/ihart_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_ihart_identicals
-
-# spark exome EX
-# srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed 
-#srun python3 parameter_estimation/pull_famgen_counts_vcf.py /oak/stanford/groups/dpwall/ihart/vcf/v34/$SLURM_ARRAY_TASK_ID.reheader.vcf.gz data/v34.vcf.ped $SLURM_ARRAY_TASK_ID counts_ihart --family_sizes 3 4 5 6 7 --depth_bins 20 30
-
-# spark exome EX25
+# WES distance from target region
 # srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX25 --include data/VCRome_2_1_hg38_primary_targets_liftover_0_25.bed
-
-# spark exome EX50
 # srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX50 --include data/VCRome_2_1_hg38_primary_targets_liftover_25_50.bed
-
-# spark exome EX75
 #srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX75 --include data/VCRome_2_1_hg38_primary_targets_liftover_50_75.bed
-
-# spark exome EX1000
 #srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX1000 --include data/VCRome_2_1_hg38_primary_targets_liftover_75_1000.bed
 
-# spark exome identicals
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark_exome data/spark_exome_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_spark_exome_EX_identicals --include data/VCRome_2_1_hg38_primary_targets_liftover_ordered.bed
-
-# spark
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark.ped $SLURM_ARRAY_TASK_ID split_gen_spark
-
-# spark identicals
-#srun python3 parameter_estimation/pull_famgen_counts.py split_gen_spark data/spark_identicals.ped $SLURM_ARRAY_TASK_ID split_gen_spark_identicals
 
 # ------------------------ For sibpair paper ------------------------
 #srun python3 parameter_estimation/pull_famgen_counts.py ../PhasingFamilies/split_gen_ihart ../PhasingFamilies/data/v34.vcf.ped.quads.ped $SLURM_ARRAY_TASK_ID ../PhasingFamilies/split_gen_ihart_quads
