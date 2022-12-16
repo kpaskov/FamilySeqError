8a2527ab3a817b0b989a059fe5a316289901ddd6
Kelley <kpaskov@stanford.edu>
2022-12-16 13:35:59 -0500

    streamling args

--- pull_famgen_counts.py
+++ pull_famgen_counts.py
@@ -1,6 +1,6 @@
 import sys
 from itertools import product
-from os import listdir
+import os
 import numpy as np
 import scipy.sparse as sparse
 import argparse
@@ -10,12 +10,23 @@ parser = argparse.ArgumentParser(description='Pull family genotype counts.')
 parser.add_argument('data_dir', type=str, help='Directory of genotype data in .npy format produced using the VCFtoNPZ project.')
 parser.add_argument('ped_file', type=str, help='Pedigree file (.ped).')
 parser.add_argument('chrom', type=str, help='Chromosome.')
+parser.add_argument('--output_dir_name', type=str, default=None, help='Name of directory to use for output.')
 parser.add_argument('--include', type=str, default=None, help='Regions to include (.bed).')
 parser.add_argument('--exclude', type=str, default=None, help='Regions to exclude (.bed).')
+parser.add_argument('--out', type=str, default=None, help='Regions to include (.bed).')
 parser.add_argument('--use_pass', action='store_true', default=False, help='If flag is present, use apply pass filter to filter out variants that do not PASS. If flag is absent, ignore PASS filter.')
 #parser.add_argument('--use_bases', action='store_true', default=False, help='Pull counts per base (ex. AA, AT) rather than per genotype (ex. 0/0, 0/1).')
 args = parser.parse_args()
 
+if not os.path.exists('%s/family_genotype_counts' % args.data_dir):
+    os.makedirs('%s/family_genotype_counts' % args.data_dir)
+
+if args.output_dir_name is None:
+    output_dir = '%s/family_genotype_counts'
+else:
+    output_dir = '%s/family_genotype_counts/%s' % (args.data_dir, args.output_dir_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
 if args.chrom == '23':
     args.chrom = 'X'
@@ -31,7 +42,7 @@ obss = ['0/0', '0/1', '1/1', './.']
 with open('%s/genotypes/info.json' % args.data_dir, 'r') as f:
     info = json.load(f)
 
-with open('%s/family_genotype_counts/info.json' % args.data_dir, 'w+') as f:
+with open(output_dir, 'w+') as f:
     json.dump(info, f)
 
 def process_bedfile(bed_file):
@@ -78,11 +89,11 @@ with open(args.ped_file, 'r') as f:
 print('families %d' % len(families))
 
 
-gen_files = sorted([f for f in listdir('%s/genotypes' % args.data_dir) if ('chr.%s.' % args.chrom) in f and 'gen.npz' in f], key=lambda x: int(x.split('.')[2]))
+gen_files = sorted([f for f in os.listdir('%s/genotypes' % args.data_dir) if ('chr.%s.' % args.chrom) in f and 'gen.npz' in f], key=lambda x: int(x.split('.')[2]))
 for gen_file in gen_files:
     batch_num = int(gen_file.split('.')[2])
 
-    out_file = '%s/family_genotype_counts/chr.%s.%d.famgen.counts.txt' % (args.data_dir, args.chrom, batch_num)
+    out_file = '%s/chr.%s.%d.famgen.counts.txt' % (output_dir, args.chrom, batch_num)
     print('saving to %s' % out_file)
 
     pos_data = np.load('%s/genotypes/chr.%s.%d.gen.coordinates.npy' % (args.data_dir, args.chrom, batch_num))
